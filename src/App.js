b470095096ec95f70041eb1624405f92aadd1961
sogood17 <100080482+sogood17@users.noreply.github.com>
2023-01-14 21:07:10 +0900

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import "./styles.css";
+import React from "react";
+
+export default function App() {
+  const [count, setCount] = React.useState(0);
+
+  function clickBtn() {
+    setCount(count + 1);
+  }
+
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
+      <h3>Here's the count!</h3>
+      <h3>↓↓↓</h3>
+      <button onClick={clickBtn}>Click me!</button>
+      <h2>{count}</h2>
+    </div>
+  );
+}
